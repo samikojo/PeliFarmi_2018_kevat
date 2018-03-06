0b0cf1d9ac298877b70785f69666c1a21f4beec5
Sami Kojo <sami.kojo@gmail.com>
2018-03-06 11:32:00 +0200

    Luento 1:
    - Assetin tuonti Asset storesta
    - PlayerSpaceship
      - käyttäjän syötteen luku
      - aluksen liikuttaminen käyttäjän syötteen perusteella

---
+++ Assets/Code/Mover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour 
+{
+	// Jäsenmuuttuja, joka määrittää aluksen nopeuden.
+	// public-määre tekee muuttujasta muokattavan Unityn puolella.
+	public float Speed;
+	
+	public void Move( Vector3 direction )
+	{
+		// Aluksen tämänhetkinen sijainti pelimaailmassa
+		Vector3 position = transform.position;
+		// Normalisoidaan suuntavektori, jotta sitä voidaan käyttää
+		// laskuissa. Normalisoitu vektori on alkuperäisen vektorin
+		// kanssa samaan suuntaan osoittava yhden mittainen vektori.
+		direction = direction.normalized;
+		
+		// Uusi sijainti on vanha sijainti + suunta * vauhti (nopeus).
+		// Nopeus pitää kertoa Time.deltaTimella (ajalla, joka on kulunut
+		// edellisen framen suorittamisesta), jotta liikuttaisimme alusta
+		// yksikköä/s eikä framea/s. Uusi sijainti sijoitetaan position
+		// muuttujaan, jolloin se korvaa vanhan sijainnin arvon.
+		position = position + direction * Speed * Time.deltaTime;
+		
+		// Kerrotaan Unitylle uusi sijainti sijoittamalla laskun tulos
+		// transform.position muuttujaan.
+		transform.position = position;
+	}
+}
---
+++ Assets/Code/PlayerSpaceship.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpaceship : MonoBehaviour {
+
+	// Jäsenmuuttuja, näkyy kaikille luokan metodeille.
+	int sum;
+	
+	// Jäsenmuuttuja, jonka tyyppi on Mover ja nimi mover.
+	Mover mover;
+
+	// Use this for initialization
+	void Start () {
+		// Metodi palauttaa lopputuloksen ja se tallennetaan
+		// muuttujaan nimeltä sum.
+		sum = Sum(1, 2);
+		
+		// Hakee Mover-tyyppista samasta GameObjectista, johon tämä
+		// PlayerSpaceship-komponentti on kytketty.
+		mover = GetComponent<Mover>();
+	}
+	
+	// Metodi, jonka Unity suorittaa automaattisesti kerran framen aikana.
+	void Update () {
+		Vector3 input = ReadInput();
+		Debug.Log(input);
+		
+		// Mover liikuttaa alusta kerran framessa siihen suuntaan, johon
+		// kayttäjä haluaa aluksen liikkuvan.
+		mover.Move(input);
+	}
+	
+	// Metodi, joka lukee käyttäjän syötteen.
+	// Syöte luetaan Horizontal ja Vertical akseleilta
+	// ja lopputulos palautetaan Vector3-tyyppisessä oliossa.
+	Vector3 ReadInput () {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		Vector3 result = new Vector3(horizontal, vertical, 0);
+		return result;
+	}
+	
+	// Metodi, joka laskee kaksi lukua yhteen.
+	// Palauttaa lopputuloksen metodin suorittajalle.
+	int Sum ( int first, int second ) 
+	{
+		int sum = first + second;
+		return sum;
+	}
+}
